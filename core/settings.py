ef3c9f4d08b3ba15117937af30e11d51f79694aa
Khổng Minh Đăng <minhdanglhp@gmail.com>
2022-01-10 15:37:08 +0700

    commit

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,12 @@ CHANNEL_LAYERS = {
         },
     },
 }
+CACHES = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisCache",
+        "LOCATION":  [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        "OPTIONS" : {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    },
+}
